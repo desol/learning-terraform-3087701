25a42cb84d65bf902a2e141d0c98455804fed886
Lee John <lee.john@cyderes.com>
2026-02-24 11:14:36 -0500

    refactor to use vars

--- variables.tf
+++ variables.tf
@@ -1,4 +1,43 @@
 variable "instance_type" {
- description = "Type of EC2 instance to provision"
- default     = "t3.nano"
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
+}
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+
+  type = object({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name  = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    owner = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type = object({
+    name           = string
+    network_prefix = string
+  })
+
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "blog_sizing" {
+  description = "the min and max number of instances in the ASG"
+
+  type = object({
+    min_size = number
+    max_size = number
+  })
+  default = {
+    min_size = 1
+    max_size = 1
+  }
 }
